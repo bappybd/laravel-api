2ebfb2995720e740282e4dc1fd14296175cffdfc
bappybd <bappybd@gmail.com>
2018-04-25 14:20:30 +0600

    Updated API

--- src/app/Http/Controllers/API/RegisterController.php
+++ src/app/Http/Controllers/API/RegisterController.php
@@ -2,13 +2,11 @@
 
 namespace App\Http\Controllers\API;
 
-
 use Illuminate\Http\Request;
 use App\Http\Controllers\API\BaseController as BaseController;
 use App\User;
 use Illuminate\Support\Facades\Auth;
 use Validator;
-
 
 class RegisterController extends BaseController
 {
@@ -26,18 +24,15 @@ class RegisterController extends BaseController
             'c_password' => 'required|same:password',
         ]);
 
-
-        if($validator->fails()){
+        if ($validator->fails()) {
             return $this->sendError('Validation Error.', $validator->errors());
         }
-
 
         $input = $request->all();
         $input['password'] = bcrypt($input['password']);
         $user = User::create($input);
-        $success['token'] =  $user->createToken('MyApp')->accessToken;
-        $success['name'] =  $user->name;
-
+        $success['token'] = $user->createToken('MyApp')->accessToken;
+        $success['name'] = $user->name;
 
         return $this->sendResponse($success, 'User register successfully.');
     }
